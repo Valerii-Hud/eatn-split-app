7af9a1ea1b79ca2b32430af1df540bd6ee822073
Valerii Hud <support@valerii-hud.com>
2025-08-08 10:18:48 +0200

    feat: created static part of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './db/data';
+import Button from './components/Button';
 import FriendsList from './components/FriendsList';
-
+import FormAddFriend from './components/FormAddFriend';
+import FormSplitBill from './components/FormSplitBill';
 export default function App() {
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,13 @@
+import Button from './Button';
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫🏻Friend name</label>
+      <input type="text" />
+
+      <label>🖼️Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
